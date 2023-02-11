109f0d252884255dfc0709a1ec11da165ed63c52
hady-bs <73134672+hady-bs@users.noreply.github.com>
2023-02-11 17:59:20 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let content = document.querySelector(".words");
-let txt = "Hello in my codepen to show my portfolio";
+let txt = "Hello in my portfolio";
 let i = 0;
 window.onload = setTimeout(function hello() {
   if (i < txt.length) {
